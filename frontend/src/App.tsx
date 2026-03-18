54669014a06335d979f34a4f4dc51aeb0ce70a6b
renatotan <renatotaniii@gmail.com>
2026-03-19 00:36:56 +0800

    fix: landing page is dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./features/layout/Layout";
 import Dashboard from "./features/dashboard/Dashboard";
 
@@ -6,10 +6,11 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index path="dashboard" element={<Dashboard />} />
-        <Route index path="add" element={<Dashboard />} />
-        <Route index path="edit" element={<Dashboard />} />
-        <Route index path="details" element={<Dashboard />} />
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="add" element={<Dashboard />} />
+        <Route path="edit" element={<Dashboard />} />
+        <Route path="details" element={<Dashboard />} />
       </Route>
     </Routes>
   );
