13a0d7d00fa06aaa148ef1e315dbdacf00be321d
renatotan <renatotaniii@gmail.com>
2026-06-12 16:20:55 +0800

    fix: sizing issues

--- frontend/src/features/layout/Layout.tsx
+++ frontend/src/features/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center">
+    <div className="w-full h-full flex flex-col justify-center">
       <div className="w-full h-full flex flex-row">
         <SideBar />
         <div className="w-full h-full bg-primary">
